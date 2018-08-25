799c7fdde93a153d8e51bfb1a2be893ea0bd5408
efimko185 <39230300+efimko185@users.noreply.github.com>
2018-08-25 21:02:15 +0300

    "chance" update

---
+++ modules/chance.py
@@ -0,0 +1,18 @@
+from modules import f, commands as c, status
+import random
+
+command = ['бот, инфа ', 'Бот, инфа ']
+c.commands += '\n"Бот, инфа <определение>" -- определяет шанс вашего определения.'
+
+def chance():
+	status.messagesSend += 1	
+	chan = event.text[10:]
+	chanc = ('Вероятность, что '+ chan + 'составляет ' + str(random.randint(0, 100)) + '%')
+	if event.from_chat:
+		f.write_msg_chat(event.chat_id, chanc)
+		f.date()   
+		print('{}[Chance, to chat {} (user {})]'.format(f.dat, event.chat_id, event.user_id))
+	else:
+		f.write_msg(event.user_id, chanc)
+		f.date()   
+		print('{}[Chance, to user {}]'.format(f.dat, event.user_id))
